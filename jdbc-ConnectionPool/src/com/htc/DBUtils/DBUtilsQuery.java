6475403cce4959b4ab24cd45a2d465414eb7bcdc
1812869775@qq.com <1812869775@qq.com>
2019-06-06 16:01:52 +0800

    QueryRunner实现查询操作

--- jdbc-ConnectionPool/src/com/htc/DBUtils/DBUtilsQuery.java
+++ jdbc-ConnectionPool/src/com/htc/DBUtils/DBUtilsQuery.java
@@ -5,10 +5,13 @@ package com.htc.DBUtils;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.junit.Test;
 
@@ -61,6 +64,7 @@ public class DBUtilsQuery {
 	/**
 	 * function: BeanListHandlerQuery
 	 * Description:BeanListHandler处理方式-将数据表的每一行数据,封装成JavaBean类对象
+	 * 									-->多行数据就多个JavaBean对象-->存储为List集合								
 	 * return:void
 	 */
 	@Test
@@ -112,7 +116,8 @@ public class DBUtilsQuery {
 	 */
 	@Test
 	public void ScalarHanderQuery() throws SQLException{
-		//通过id查询详情，将查询结果封装到JavaBean product
+		//ScalarHandler:用于处理聚合函数执行结果(一行一列)
+		//查询总记录数
 		//1.核心类
 		QueryRunner queryRunner = new QueryRunner(C3P0Utils.getDataSource());
 		//2.sql语句
@@ -120,12 +125,15 @@ public class DBUtilsQuery {
 		//3.实际参数
 		Object[] params = {};
 		//4.查询并封装
-		//int r = queryRunner.query(sql, new ScalarHandler<int>());   //这句错误(int) 
-		//错误代码：new ScalarHandler<int>()
+		//long r = queryRunner.query(sql, new ScalarHandler<long>());   //这句错误(long)
+		//错误代码：new ScalarHandler<long>() / new ScalarHandler<int>()
+		//(1)返回的是long型，如果用int:
+		//java.lang.ClassCastException: java.lang.Long cannot be cast to java.lang.Integer
+		//(2)泛型必须是包装类型，而不能是基本类型（包括基本类型的数组），如果不用包装类型：
 		//java报错：Syntax error, insert "Dimensions" to complete ReferenceType(语法错误，插入“维度”以完成引用类型)
-		//原因：泛型必须是包装类类型，而不能是基本类型（包括基本类型的数组）
-		//修改为：new ScalarHandler<int>()
-		Integer r = queryRunner.query(sql, new ScalarHandler<Integer>());
+
+		//修改为：new ScalarHandler<Long>()
+		Long r = queryRunner.query(sql, new ScalarHandler<Long>());
 		
 		System.out.println(r);
 		
@@ -134,5 +142,127 @@ public class DBUtilsQuery {
 	}
 	
 	
+	/**
+	 * function: MapHandlerQuery
+	 * Description:MapHandler处理方式-将数据表结果集的一行数据封装成Map集合
+	 * 				键: 数据表中的列
+	 * 				值: 这个列中的数据
+	 * return:void
+	 */
+	@Test
+	public void MapHandlerQuery() throws SQLException{
+		//MapHandler:将查询到的一条记录，封装到Map中,map.key=字段名,map.value=值
+		//注意与BeanHandler查询一行数据的区别
+		
+		//1.核心类
+		QueryRunner queryRunner = new QueryRunner(C3P0Utils.getDataSource());
+		//2.sql语句
+		String sql = "select * from product where pid = ?";
+		//3.实际参数
+		Object[] params = {6};
+		//4.查询并封装
+		Map<String, Object> map = queryRunner.query(sql, new MapHandler(), params);
+		//5.打印对象
+		System.out.println(map);
+		//map
+		//{pid=6, pname=OPPO, price=3400.0, category_id=c002}
+		//product
+		//Product [pid=6, pname=OPPO, price=3400.0, category_id=c002]
+		
+		System.out.println(map.get("pid"));
+		System.out.println(map.get("pname"));
+		System.out.println(map.get("price"));
+		System.out.println(map.get("category_id"));
+		
+//		6
+//		OPPO
+//		3400.0
+//		c002
+
+	}
+	
+	/**
+	 * function: MapListHandlerQuery
+	 * Description:MapListHandler处理方式-将数据表的结果集的每一行封装成Map集合
+	 * 				 数据表有多行数据,出现多个Map集合,存储到List集合
+	 * return:void
+	 */
+	@Test
+	public void MapListHandlerQuery() throws SQLException{
+		//MapHandler:将查询到的一条记录，封装到Map中,map.key=字段名,map.value=值
+		//注意与BeanHandler查询一行数据的区别
+		
+		//1.核心类
+		QueryRunner queryRunner = new QueryRunner(C3P0Utils.getDataSource());
+		//2.sql语句
+		String sql = "select * from product";
+		//3.实际参数
+		Object[] params = {};
+		//4.查询并封装
+		//Map<String, Object> map = queryRunner.query(sql, new MapHandler(), params);
+		List<Map<String, Object>> list = queryRunner.query(sql, new MapListHandler());
+		
+		System.out.println(list);
+		
+//		[{pid=1, pname=IBM, price=5500.0, category_id=c001}, 
+//		 {pid=2, pname=长城, price=3200.0, category_id=c001}, 
+//		 {pid=3, pname=惠普, price=5000.0, category_id=c001}, 
+//		 {pid=4, pname=华为, price=3800.0, category_id=c002}, 
+//		 {pid=5, pname=小米, price=2000.0, category_id=c002}, 
+//		 pid=6, pname=OPPO, price=3400.0, category_id=c002}, 
+//		 {pid=7, pname=NOKIA, price=2000.0, category_id=c002}, 
+//		 {pid=8, pname=佰草集, price=800.0, category_id=c003}, 
+//		 {pid=9, pname=玉兰油, price=200.0, category_id=c003}, 
+//		 {pid=10, pname=大宝, price=5.0, category_id=c003}
+//		]
+		
+		
+		//5.打印对象
+		for(Map<String, Object> map : list){
+			System.out.println(map);
+		}
+		
+//		{pid=1, pname=IBM, price=5500.0, category_id=c001}
+//		{pid=2, pname=长城, price=3200.0, category_id=c001}
+//		{pid=3, pname=惠普, price=5000.0, category_id=c001}
+//		{pid=4, pname=华为, price=3800.0, category_id=c002}
+//		{pid=5, pname=小米, price=2000.0, category_id=c002}
+//		{pid=6, pname=OPPO, price=3400.0, category_id=c002}
+//		{pid=7, pname=NOKIA, price=2000.0, category_id=c002}
+//		{pid=8, pname=佰草集, price=800.0, category_id=c003}
+//		{pid=9, pname=玉兰油, price=200.0, category_id=c003}
+//		{pid=10, pname=大宝, price=5.0, category_id=c003}
+		
+		//打印出pname字段的值
+		for(Map<String, Object> map : list){
+			Object pnames = map.get("pname");
+			System.out.println(pnames);
+		}
+
+	}
+	
+	/**
+	 * function: ArrayHandlerQuery
+	 * Description:ArrayHandler处理方式-将数据表中的一行数据,存储到对象数组Object[]中
+	 * 				注意:获取查询后的一行数据,如果查询不到结果集,返回的对象数组的length=0
+	 * return:void
+	 */
+	@Test
+	public void ArrayHandlerQuery() throws SQLException{
+		//MapHandler:将查询到的一条记录，封装到Map中,map.key=字段名,map.value=值
+		//注意与BeanHandler查询一行数据的区别
+		
+		//1.核心类
+		QueryRunner queryRunner = new QueryRunner(C3P0Utils.getDataSource());
+		//2.sql语句
+		String sql = "select * from product";
+		//3.实际参数
+		Object[] params = {};
+		//4.查询并封装
+		//Map<String, Object> map = queryRunner.query(sql, new MapHandler(), params);
+		List<Map<String, Object>> list = queryRunner.query(sql, new MapListHandler());
+		
+		System.out.println(list);
+	}	
 	
 }
